5a75feb38dbcd8bc78c7d696c0ae1e7a0a702816
Alex Mironov <bermar2016@yandex.ru>
2021-06-02 13:42:07 +0300

    fix kernel

--- prefix_sum_kernel.cl
+++ prefix_sum_kernel.cl
@@ -2,19 +2,19 @@ __kernel void prefix_sum(__global float* A, __global float* B, __global float* m
     uint global_id = get_global_id(0);
     uint local_id = get_local_id(0);
 
-    __local float temp[64];
-    temp[local_id] = A[global_id];
+    __local float cache[64];
+    cache[local_id] = A[global_id];
 
     for (int i = 1; i < 64; i <<= 1) {
         barrier(CLK_LOCAL_MEM_FENCE);
-        temp[local_id] += temp[local_id - i];
+        cache[local_id] += cache[local_id - i];
     }
 	
 	barrier(CLK_LOCAL_MEM_FENCE);
-    B[global_id] = temp[local_id];
+    B[global_id] = cache[local_id];
 	
-	if(local_id == 64-1) {
-		max_sum[get_group_id(0)] = temp[local_id];
+	if(local_id == 63) {
+		max_sum[get_group_id(0)] = cache[local_id];
 	}
 }
 
@@ -23,12 +23,10 @@ __kernel void total_prefix_sum(__global float* B, __global float* max_sum) {
     uint group_id = get_group_id(0);
     uint groups_count = get_num_groups(0) - 1;
 
-	if(group_id != 0) {
 		for(int i = 0; i < groups_count; i++) {
-			if(group_id < i) {
+			if(group_id <= i) {
 				return;
 			}
 			B[global_id] += max_sum[i];
-		}
 	}
 }
--- prefix_sum_opencl.cpp
+++ prefix_sum_opencl.cpp
@@ -10,9 +10,21 @@ using std::chrono::high_resolution_clock;
 using std::chrono::duration;
 using std::chrono::steady_clock;
 
-#define MAX_SOURCE_SIZE (0x100000)
+#define BUFFER_SIZE (0x100000)
 
-int main(void) {
+int main(int argc, char* argv[]) {
+
+    if (argc != 4) {
+        std::cerr << "Invalid args count" << std::endl;
+        return 1;
+    }
+
+    int global_id_device = atoi(argv[1]);
+    if ((!global_id_device && strcmp(argv[1], "0") != 0) || global_id_device < -1) {
+        std::cerr << "Invalid threads count arg" << std::endl;
+        return 1;
+    }
+    
     cl_uint platformCount;
     cl_platform_id* platforms;
     cl_uint deviceCount;
@@ -22,7 +34,7 @@ int main(void) {
     std::vector <cl_device_id> gpu_nondescrete_devices;
     cl_ulong device_host_unified_memory;
     cl_device_type device_type;
-    cl_int ret;
+    cl_int err;
 
     if (clGetPlatformIDs(0, NULL, &platformCount) != CL_SUCCESS) {
         std::cerr << "Read platforms count error" << std::endl;
@@ -93,10 +105,10 @@ int main(void) {
 
     delete[] platforms;
 
-    std::string file_path = "C:\\Users\\Alex\\source\\repos\\ConsoleApplication1\\test1.txt";//argv[1];
+    std::string file_path_in = argv[2];
     int n = 0;
     float* A;
-    std::ifstream in(file_path);
+    std::ifstream in(file_path_in);
 
     if (in.is_open())
     {
@@ -125,7 +137,7 @@ int main(void) {
     }
     else
     {
-        std::cerr << "Can not read file " << file_path << std::endl;
+        std::cerr << "Can not read file " << file_path_in << std::endl;
         return 1;
     }
     in.close();
@@ -134,25 +146,28 @@ int main(void) {
     char* source_str;
     size_t source_size;
 
-    fopen_s(&fp, "C:\\Users\\Alex\\source\\repos\\ConsoleApplication1\\2.cl", "r");
+    fopen_s(&fp, "prefix_sum_kernel.cl", "r");
     if (!fp) {
-        fprintf(stderr, "Failed to load kernel.\n");
+        std::cerr << "Failed to load kernel " << "prefix_sum_kernel.cl" << std::endl;
+        delete[] A;
         return 1;
     }
-    source_str = new char [MAX_SOURCE_SIZE];
+    source_str = new char [BUFFER_SIZE];
     if (!source_str) {
         std::cerr << "Can not allocate memory" << std::endl;
         delete[] A;
         delete[] source_str;
         return 1;
     }
-    source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
+    source_size = fread(source_str, 1, BUFFER_SIZE, fp);
     fclose(fp);
 
 
-    int global_id_device = 1;
     cl_device_id* device_id = NULL;
     if (global_id_device < 0) {
+        std::cerr << "Invalid device id" << std::endl;
+        delete[] source_str;
+        delete[] A;
         return 1;
     }
     if (global_id_device < gpu_descrete_devices.size()) {
@@ -165,6 +180,9 @@ int main(void) {
         device_id = &cpu_devices[global_id_device - gpu_nondescrete_devices.size() - gpu_descrete_devices.size()];
     }
     else {
+        std::cerr << "Invalid device id" << std::endl;
+        delete[] source_str;
+        delete[] A;
         return 1;
     }
 
@@ -180,73 +198,212 @@ int main(void) {
     }
 
     steady_clock::time_point start = high_resolution_clock::now();
-    cl_context context = clCreateContext(NULL, 1, device_id, NULL, NULL, &ret);
-
-    cl_command_queue command_queue = clCreateCommandQueue(context, *device_id, CL_QUEUE_PROFILING_ENABLE, &ret);
-
-    cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, n * sizeof(float), NULL, &ret);
-    cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_WRITE, n * sizeof(float), NULL, &ret);
-    cl_mem max_sum_mem_obj = clCreateBuffer(context, CL_MEM_READ_WRITE, global_groups_size * sizeof(float), NULL, &ret);
-
-    ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0, n * sizeof(float), A, 0, NULL, NULL);
-
-    cl_program program = clCreateProgramWithSource(context, 1, (const char**)&source_str, (const size_t*)&source_size, &ret);
-
-    ret = clBuildProgram(program, 1, device_id, NULL, NULL, NULL);
-
-    if (ret == CL_BUILD_PROGRAM_FAILURE
-#ifdef DEBUG
-        || errCode == 0
-#endif
-        ) {
-        size_t clBuildInfoLogSize = 0;
-        clGetProgramBuildInfo(program, *device_id, CL_PROGRAM_BUILD_LOG, 0, NULL, &clBuildInfoLogSize);
-        char* buildInfoLog = new char[clBuildInfoLogSize];
-        clGetProgramBuildInfo(program, *device_id, CL_PROGRAM_BUILD_LOG, clBuildInfoLogSize, buildInfoLog, &clBuildInfoLogSize);
-        printf("Compiler response: %s", buildInfoLog);
-        free(buildInfoLog);
+    cl_context context = clCreateContext(NULL, 1, device_id, NULL, NULL, &err);
+    if (err != CL_SUCCESS) {
+        std::cerr << "Can not create context" << std::endl;
+        delete[] source_str;
+        delete[] A;
+        return 1;
     }
 
-    cl_kernel kernel = clCreateKernel(program, "prefix_sum", &ret);
-    ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&a_mem_obj);
-    ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&b_mem_obj);
-    ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)&max_sum_mem_obj);
+    cl_command_queue command_queue = clCreateCommandQueue(context, *device_id, CL_QUEUE_PROFILING_ENABLE, &err);
+    if (err != CL_SUCCESS) {
+        std::cerr << "Can not create command queue" << std::endl;
+        delete[] source_str;
+        delete[] A;
+        return 1;
+    }
 
-    cl_kernel kernel1 = clCreateKernel(program, "total_prefix_sum", &ret);
-    ret = clSetKernelArg(kernel1, 0, sizeof(cl_mem), (void*)&b_mem_obj);
-    ret = clSetKernelArg(kernel1, 1, sizeof(cl_mem), (void*)&max_sum_mem_obj);
+    cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, n * sizeof(float), NULL, &err);
+    if (err != CL_SUCCESS) {
+        std::cerr << "Can not create buffer" << std::endl;
+        delete[] source_str;
+        delete[] A;
+        return 1;
+    }
+    cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_WRITE, n * sizeof(float), NULL, &err);
+    if (err != CL_SUCCESS) {
+        std::cerr << "Can not create buffer" << std::endl;
+        delete[] source_str;
+        delete[] A;
+        return 1;
+    }
+    cl_mem max_sum_mem_obj = clCreateBuffer(context, CL_MEM_READ_WRITE, global_groups_size * sizeof(float), NULL, &err);
+    if (err != CL_SUCCESS) {
+        std::cerr << "Can not create buffer" << std::endl;
+        delete[] source_str;
+        delete[] A;
+        return 1;
+    }
+
+    if (clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0, n * sizeof(float), A, 0, NULL, NULL) != CL_SUCCESS) {
+        std::cerr << "Can not write buffer" << std::endl;
+        delete[] source_str;
+        delete[] A;
+        return 1;
+    }
+
+    cl_program program = clCreateProgramWithSource(context, 1, (const char**)&source_str, (const size_t*)&source_size, &err);
+    if (err != CL_SUCCESS) {
+        std::cerr << "Can not create program" << std::endl;
+        delete[] source_str;
+        delete[] A;
+        return 1;
+    }
+
+    delete[] source_str;
+    if (clBuildProgram(program, 1, device_id, NULL, NULL, NULL) != CL_SUCCESS) {
+        std::cerr << "Can not create buffer" << std::endl;
+        delete[] A;
+        return 1;
+    }
+
+    cl_kernel kernel = clCreateKernel(program, "prefix_sum", &err);
+    if (err != CL_SUCCESS) {
+        std::cerr << "Can not create kernel" << std::endl;
+        delete[] A;
+        return 1;
+    }
+    if (clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&a_mem_obj) != CL_SUCCESS) {
+        std::cerr << "Can not set argument" << std::endl;
+        delete[] A;
+        return 1;
+    }
+    if (clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&b_mem_obj) != CL_SUCCESS) {
+        std::cerr << "Can not set argument" << std::endl;
+        delete[] A;
+        return 1;
+    }
+    if (clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)&max_sum_mem_obj) != CL_SUCCESS) {
+        std::cerr << "Can not set argument" << std::endl;
+        delete[] A;
+        return 1;
+    }
+
+    cl_kernel kernel1 = clCreateKernel(program, "total_prefix_sum", &err);
+    if (err != CL_SUCCESS) {
+        std::cerr << "Can not create kernel" << std::endl;
+        delete[] A;
+        return 1;
+    }
+    if (clSetKernelArg(kernel1, 0, sizeof(cl_mem), (void*)&b_mem_obj) != CL_SUCCESS) {
+        std::cerr << "Can not set argument" << std::endl;
+        delete[] A;
+        return 1;
+    }
+    if (clSetKernelArg(kernel1, 1, sizeof(cl_mem), (void*)&max_sum_mem_obj) != CL_SUCCESS) {
+        std::cerr << "Can not set argument" << std::endl;
+        delete[] A;
+        return 1;
+    }
    
-    cl_event kernel_event;
-    ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_item_size, &local_item_size, 0, NULL, &kernel_event);
-    ret = clEnqueueNDRangeKernel(command_queue, kernel1, 1, NULL, &global_item_size, &local_item_size, 0, NULL, NULL);
+    cl_event kernel_event, kernel_event1;
+    if (clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_item_size, &local_item_size, 0, NULL, &kernel_event) != CL_SUCCESS) {
+        std::cerr << "Can not start kernel" << std::endl;
+        delete[] A;
+        return 1;
+    }
+    if (clEnqueueNDRangeKernel(command_queue, kernel1, 1, NULL, &global_item_size, &local_item_size, 0, NULL, &kernel_event1) != CL_SUCCESS) {
+        std::cerr << "Can not start kernel" << std::endl;
+        delete[] A;
+        return 1;
+    }
 
     float* B = new float[n];
-    ret = clEnqueueReadBuffer(command_queue, b_mem_obj, CL_TRUE, 0, n * sizeof(float), B, 0, NULL, NULL);
+    if (!B) {
+        std::cerr << "Can not allocate memory" << std::endl;
+        delete[] B;
+        delete[] A;
+        return 1;
+    }
+    if (clEnqueueReadBuffer(command_queue, b_mem_obj, CL_TRUE, 0, n * sizeof(float), B, 0, NULL, NULL) != CL_SUCCESS) {
+        std::cerr << "Can not read buffer " << err << std::endl;
+        delete[] B;
+        delete[] A;
+        return 1;
+    }
 
     steady_clock::time_point end = high_resolution_clock::now();
     duration<double, std::milli> ms_double = end - start;
 
     cl_ulong begin_k = NULL, end_k = NULL;
     cl_ulong total_time = 0;
-    ret = clGetEventProfilingInfo(kernel_event, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &begin_k, NULL);
-    ret = clGetEventProfilingInfo(kernel_event, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &end_k, NULL);
+    if (clGetEventProfilingInfo(kernel_event, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &begin_k, NULL) != CL_SUCCESS) {
+        std::cerr << "Can not get profiling info" << std::endl;
+        delete[] B;
+        delete[] A;
+        return 1;
+    }
+    if (clGetEventProfilingInfo(kernel_event, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &end_k, NULL) != CL_SUCCESS) {
+        std::cerr << "Can not get profiling info" << std::endl;
+        delete[] B;
+        delete[] A;
+        return 1;
+    }
+    total_time += end_k - begin_k;
+    if (clGetEventProfilingInfo(kernel_event1, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &begin_k, NULL) != CL_SUCCESS) {
+        std::cerr << "Can not get profiling info" << std::endl;
+        delete[] B;
+        delete[] A;
+        return 1;
+    }
+    if (clGetEventProfilingInfo(kernel_event1, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &end_k, NULL) != CL_SUCCESS) {
+        std::cerr << "Can not get profiling info" << std::endl;
+        delete[] B;
+        delete[] A;
+        return 1;
+    }
     total_time += end_k - begin_k;
 
-    for (int i = 0; i < n; i++)
-        printf("%f\n", B[i]);
+    std::string file_path_out = argv[3];
+    std::ofstream out(file_path_out);
+    if (out.is_open())
+    {
+        for (int i = 0; i < n; i++)
+            out << B[i] << " ";
+    }
+    else
+    {
+        std::cerr << "Can not read file " << file_path_out << std::endl;
+        return 1;
+    }
+    out.close();
 
     printf("\nTime: %f\t%f \n", total_time * 1.0 / 1000000, ms_double.count());
 
-    ret = clFlush(command_queue);
-    ret = clFinish(command_queue);
-    ret = clReleaseKernel(kernel);
-    ret = clReleaseProgram(program);
-    ret = clReleaseMemObject(a_mem_obj);
-    ret = clReleaseMemObject(b_mem_obj);
-    ret = clReleaseCommandQueue(command_queue);
-    ret = clReleaseContext(context);
-    delete [] A;
-    delete [] B;
-    delete [] source_str;
+    delete[] A;
+    delete[] B;
+    if(clReleaseKernel(kernel) != CL_SUCCESS) {
+        std::cerr << "Can not release kernel" << std::endl;
+        return 1;
+    }
+    if (clReleaseKernel(kernel1) != CL_SUCCESS) {
+        std::cerr << "Can not release kernel" << std::endl;
+        return 1;
+    }
+    if (clReleaseProgram(program) != CL_SUCCESS) {
+        std::cerr << "Can not release program" << std::endl;
+        return 1;
+    }
+    if(clReleaseMemObject(a_mem_obj) != CL_SUCCESS) {
+        std::cerr << "Can not release mem object" << std::endl;
+        return 1;
+    }
+    if(clReleaseMemObject(b_mem_obj) != CL_SUCCESS) {
+        std::cerr << "Can not release mem object" << std::endl;
+        return 1;
+    }
+    if(clReleaseMemObject(max_sum_mem_obj) != CL_SUCCESS) {
+        std::cerr << "Can not release mem object" << std::endl;
+        return 1;
+    }
+    if(clReleaseCommandQueue(command_queue) != CL_SUCCESS) {
+        std::cerr << "Can not release command queue" << std::endl;
+        return 1;
+    }
+    if(clReleaseContext(context) != CL_SUCCESS) {
+        std::cerr << "Can not release context" << std::endl;
+        return 1;
+    }
     return 0;
 }
